50f8b7b9a8112ddc73c830a709232d4da218fa94
asherlaw <asherlaw@pepinos-air.lan>
2022-07-09 21:04:23 -0700

    Done 9:03 PM at home

--- src/_99_extra/pong/Pong.java
+++ src/_99_extra/pong/Pong.java
@@ -13,28 +13,52 @@ public class Pong extends PApplet {
     int ySpeed = 2;
     int ballX = 0;
     int ballY = 0;
+    int paddleX = 0;
+    int paddleY = 329;
+    int paddleLength = 110;
+    
+    int Score = 0;
     @Override
     public void settings() {
         size(660, 371);
-	
     }
+    
     @Override
     public void setup() {
-    	
     }
+    
+    boolean intersects(int ballX, int ballY, int paddleX, int paddleY, int paddleLength) {
+    	return (ballY > paddleY && ballX > paddleX && ballX < paddleX + paddleLength);
+	}
 
     @Override
     public void draw() {
+    	paddleX = mouseX;
+    	
+    	if (intersects(ballX,ballY,paddleX,paddleY,paddleLength)) {
+    		ySpeed = -ySpeed;
+    		Score = Score+1;
+    	}
+    	
+    	if (ballY < 0) ySpeed = -ySpeed;
+    	if (ballY>371) JOptionPane.showMessageDialog(null, "Your score was "+Score+".");
+    	if(ballX > width) xSpeed = -xSpeed;
+    	if(ballX < 0) xSpeed = -xSpeed;
+    	
     	ballX=ballX+xSpeed;
     	ballY=ballY+ySpeed;
     	background(190,190,190);
+    	
     	noStroke();
     	fill(180, 0, 0); //in draw method
         ellipse(ballX, ballY, 20, 20); //in draw method
+        fill (0,0,130);
+        
+        rect(paddleX, paddleY, paddleLength, 30);
         stroke(250, 250, 0); //in draw method
     }
 
-    static public void main(String[] args) {
+	static public void main(String[] args) {
         PApplet.main(Pong.class.getName());
     }
     
